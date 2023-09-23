18b338c58936c415ebe58180fe3e7265e616f487
Valiec <valiec2019@gmail.com>
2023-09-23 18:55:11 -0400

    CPU/RAM status.

--- status.sh
+++ status.sh
@@ -1,13 +1,14 @@
 while true; do
 	health_str=$(docker exec "$1"  mc-health | sed "s/localhost:25565 : //" | sed -E "s/version=([^ ]+) online=([^ ]+) max=([^ ]+) motd='(.+)'/Version: \1\nPlayers Online: \2\/\3\nMOTD: \4/g")
-	list_str=$(docker exec "$1" rcon-cli list | sed -E "s/There are [0-9]+ of a max of [0-9]+ players online:[ ]+//" | sed "s/, /,/g" | tr ", " "\n")
+	cpu_ram_str=$(docker exec "$1" top -b -n 1 -E k | grep java | awk '{ print "Total Memory: " $5/1024, "MiB\nPhysical Memory: " $6, "\n%CPU: " $9 }')
+        list_str=$(docker exec "$1" rcon-cli list | sed -E "s/There are [0-9]+ of a max of [0-9]+ players online:[ ]+//" | sed "s/, /,/g" | tr ", " "\n")
 	player_line_count=$(echo "$health_str" | head -n 2 | tail -n 1 | sed -E "s/Players Online: ([0-9]+)\/[0-9]+/\1/")
-        printf "$health_str\n"
+        printf "%s\n%s\n" "$health_str" "$cpu_ram_str"
         if [ $player_line_count -gt 0 ]; then
         	printf "\nPlayers online:\n$list_str\n"
 	        player_line_count=$((player_line_count+2))
         fi
-        line_count=$((player_line_count + 3))
+        line_count=$((player_line_count + 6))
 	sleep 5
 	for i in $(seq 1 "$line_count"); do
 		printf "\r$(tput el)$(tput cuu1)"
