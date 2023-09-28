a9e9c1710fba9b26dfb80809d0be238e3183b189
Valiec <valiec2019@gmail.com>
2023-09-27 23:17:57 -0400

    Started setting up server install

--- util/install_server.sh
+++ util/install_server.sh
@@ -10,11 +10,12 @@ server_type="vanilla"
 data_dir="$1"
 
 case "$server_type" in 
-	"FORGE") ;;
-	"PAPER") ;;
-	"SPIGOT") ;;
-	"QUILT") ;;
-	"PURPUR") ;;
-	"FABRIC") ;;
+	"FORGE") "$MCCLI_DIR/mc-image-helper/bin/mc-image-helper" install-forge --minecraft-version "$mc_version" --output-directory "$data_dir" ;;
+	"PAPER") "$MCCLI_DIR/mc-image-helper/bin/mc-image-helper" install-paper --version "$mc_version" --output-directory "$data_dir" ;;
+	"SPIGOT") echo "spigot (I need to do buildtools)" ;;
+	"QUILT") echo "quilt (I need to run the installer JAR (and maybe install vanilla? idk if it does that for me))" ;;
+	"PURPUR") "$MCCLI_DIR/mc-image-helper/bin/mc-image-helper" install-purpur --version "$mc_version" --output-directory "$data_dir" ;;
+	"FABRIC") echo "fabric (I need to run the installer JAR (and maybe install vanilla? idk if it does that for me))" ;;
+	"VANILLA") echo "vanilla (I need to parse Mojang's JSON and download the right JAR)" ;;
 	*) echo "Invalid server type $server_type" >&2 ;;
 esac
