469a309a7570dc1f29c995fadcf1d9406ff60ef5
Valiec <valiec2019@gmail.com>
2023-09-30 06:39:09 -0400

    Working on download-vanilla logic.

---
+++ util/download_vanilla.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -e
+
+download_dir="$1"
+version="$2"
+
+echo "Downloading version manifest..."
+
+download_data=$(curl -# "https://launchermeta.mojang.com/mc/game/version_manifest.json" | python3 "$SCRIPT_ROOT/util/parse_mojang_json.py" "$version")
+download_hash="$(cut -f 1 <<< "$download_data")"
+download_url="$(cut -f 2- <<< "$download_data")"
+
+cd "$download_dir"
+
+echo "Downloading $version JAR..."
+
+curl -# -o "$version"_server.jar "$download_url"
+downloaded_shasum="$(sha1sum "$version"_server.jar | cut -f 1 -d " ")"
+
+if [ "$download_hash" != "$downloaded_shasum" ]; then
+    echo "mccli: warning: sha1 hash of downloaded file ($downloaded_shasum) does not match provided sum from Mojang ($download_hash)" >&2
+    read -p "continue? [Y/n]: " answer
+    if [ "$answer" != "Y" ]; then
+        echo "Exiting." >&2
+    fi
+fi
