900e8c81393c5be162ffa3899dddc4ff02b27216
Valiec <valiec2019@gmail.com>
2023-10-22 21:32:34 -0400

    Improved uninstall script.

--- uninstall.sh
+++ uninstall.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
 
+mccli_path="$(which mccli)"
+
 set -e
 
-unlink "$(which mccli)"
+if [ "$mccli_path" = "" ] && [ ! -e "$(dirname "$(readlink -f "$0")")/mccli.sh" ]; then
+	echo "mccli: cannot uninstall, mccli is not installed, exiting";
+	exit 1;
+fi
 
-rm "$(dirname "$(readlink -f "$0")")/mccli.sh"
+if [ ! "$mccli_path" = "" ]; then
+	unlink "$mccli_path"
+fi
+
+if [ -e "$(dirname "$(readlink -f "$0")")/mccli.sh" ]; then
+	rm "$(dirname "$(readlink -f "$0")")/mccli.sh"
+fi
 
 echo "mccli: sucessfully uninstalled"
